254b03ab0b0382529a1cb448dd78b56f21435f7f
FACO <hola@faco.cl>
2017-05-17 10:40:56 -0400

    +

--- faq.php
+++ faq.php
@@ -2,12 +2,24 @@
 <section>
 <h2>Preguntas frecuentes</h2>
 <?php
-$blog = array_map('str_getcsv', file('url'));
-array_walk($blog, function(&$a) use ($blog) {$a = array_combine($blog[0], $a);});
-array_shift($blog);
+$faq = array_map('str_getcsv', file('data/faq.csv'));
+array_walk($faq, function(&$a) use ($faq) {$a = array_combine($faq[0], $a);});
+array_shift($faq);
+$all = count($faq);
+for ($n=0; $n < $all; $n++){
 ?>
-<hr>
-<h4>faq_pregunta</h4>
-<p>faq_respuesta</p>
+<!--esto se debe repetir, ojo con el nro. junto al collapse_-->
+<h4>
+<a role="button" data-toggle="collapse" href="#collapse<?print $n?>" aria-expanded="false" aria-controls="collapse<?print $n?>">
+<?php echo $faq[$n]['faq_pregunta'];?>
+</a></h4>
+<div class="collapse" id="collapse<?print $n?>">
+<div class="well">
+<p><?php echo $faq[$n]['faq_respuesta'];?></p>
+</div>
+</div>
+<!--hasta acá lo que se repite-->
+<?php };?>
+
 </section>
 <?php include('footer.php');?>
